f5f4c1424d4971fe0da6f91931975414d7b52ea6
harlynton <harlynton.castano@gmail.com>
2022-07-27 16:10:04 -0500

    Adding node-argon2 to the project in order to hashing password. Adding relation between users and bookmarks. Creation of signup service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,42 @@
+import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from './../prisma/prisma.service';
-import { Injectable, Post } from "@nestjs/common";
-
+import { ForbiddenException, Injectable, Post } from "@nestjs/common";
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable()
 export class AuthService{
     constructor(private prisma: PrismaService){}
 
     @Post('signup')
-    signup(){
-        
-        return {msg:"I have signed up!"};
+    async signup(dto: AuthDto){
+        //1. Generate the password hash:
+        const hash = await argon.hash(dto.password);
+
+        //2. Save the new user in the DB:
+        try {
+            const user = await this.prisma.user.create({
+                data:{
+                    email:dto.email,
+                    hash,
+                }
+            });
+            delete user.hash
+
+            //3. Return the saved user:
+            return user;
+            
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException(
+                        'Credentials already taken'
+                    )
+                }
+            }
+            throw error
+        }
+
     }
 
     signin(){
