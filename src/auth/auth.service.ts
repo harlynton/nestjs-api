c3e263d823d0d1fd36f6f2956069fc667e6e195d
harlynton <harlynton.castano@gmail.com>
2022-07-29 18:00:03 -0500

    Creation of signin service. Adding script for docker restart in package.json. Configuration of modules. Using passport.js & JWT module for authentication.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { AuthDto } from './dto/auth.dto';
 import { PrismaService } from './../prisma/prisma.service';
 import { ForbiddenException, Injectable, Post } from "@nestjs/common";
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService{
-    constructor(private prisma: PrismaService){}
+    constructor(
+        private prisma: PrismaService, 
+        private jwt: JwtService,
+        private config: ConfigService
+    ){}
 
     @Post('signup')
     async signup(dto: AuthDto){
@@ -21,10 +27,6 @@ export class AuthService{
                     hash,
                 }
             });
-            delete user.hash
-
-            //3. Return the saved user:
-            return user;
             
         } catch (error) {
             if (error instanceof PrismaClientKnownRequestError) {
@@ -39,7 +41,42 @@ export class AuthService{
 
     }
 
-    signin(){
-        return {msg:"I have signed in!"};
+    async signin(dto: AuthDto){
+        //1. Find the user by email. If user does not exist, throw exception.
+        const user = 
+            await this.prisma.user.findUnique({
+                where:{
+                    email:dto.email
+                }
+            });
+            if (!user) throw new ForbiddenException('Credentials are incorrect!');
+
+        //2. Compare provided password vs. stored password. If provided password is incorrect, throw exception.
+        const pwdMatches =await argon.verify(user.hash, dto.password)
+        if(!pwdMatches) throw new ForbiddenException('Credentials are incorrect!')
+        
+        //3. Send back the user.
+        return this.signToken(user.id, user.email)
+    }
+
+    async signToken(userId: number, email: string): Promise<{ access_token: string }> {
+        const payload = {
+            sub: userId,
+            email
+        };
+        const secret = this.config.get('JWT_SECRET')
+
+        const token = await this.jwt.signAsync(
+            payload,
+            {
+                expiresIn: '15m',
+                secret: secret,
+            },
+        );
+
+
+        return {
+            access_token: token,
+        }
     }
 }
